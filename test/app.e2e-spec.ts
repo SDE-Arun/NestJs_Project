620ca1c21f3e27d9adc4d8d3f8f22cc4ec036692
Arun <arun@eazydiner.com>
2025-01-17 16:43:50 +0530

    test(e2e): add test cases and configuration and add container-setup.yml for Postgres service
    - Write e2e test cases for specific scenarios
    - Resolve jest-e2e.config.ts reference in package.json
    - Refactor code structure in test-utils.ts for better readability and maintainability
    - Create container-setup.yml to run Postgres container independently
    - Placeholder step to confirm health check without running tests or Prisma

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, HttpStatus } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { faker } from '@faker-js/faker/.';
+import { UserOutput } from '../src/interfaces';
+import { UserService } from '../src/user/service/user.service';
+import { AuthInputDTO } from '../src/auth/dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let userService: UserService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,12 +17,66 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    userService = moduleFixture.get<UserService>(UserService);
     await app.init();
   });
 
-  describe('user Controller', () => {
-    it('/user', () => {
-      return request(app.getHttpServer()).get('/user').expect(200).expect('Hello Guys');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('just for checking that, everything works fine or not ', () => {
+    return request(app.getHttpServer()).get('/user').expect(200).expect('Hello Guys');
+  });
+
+  describe('User Controller', () => {
+    const USER_INFO_PATH = '/user/info';
+    const mockToken = faker.internet.jwt();
+    let mockOutput: UserOutput;
+    beforeEach(() => {
+      mockOutput = {
+        id: faker.database.mongodbObjectId(),
+        email: faker.internet.email(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.future(),
+      };
+    });
+
+    it('returns UNAUTHORIZED {401} for missing token', async () => {
+      await request(app.getHttpServer()).get(USER_INFO_PATH).expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it('returns UNAUTHORIZED {401} for invalid token', async () => {
+      const invalidToken = 'invalid.jwt.token';
+      await request(app.getHttpServer())
+        .get(USER_INFO_PATH)
+        .set('Authorization', `Bearer ${invalidToken}`)
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+  });
+
+  describe('signUp in Auth Controller', () => {
+    const AUTH_SIGNUP_PATH = '/auth/signup';
+
+    it('return {500}INTERNAL_SERVER_ERROR, if password is not present in input', () => {
+      const input = {
+        email: faker.internet.email(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+      };
+      return request(app.getHttpServer()).post(AUTH_SIGNUP_PATH).send(input).expect(HttpStatus.INTERNAL_SERVER_ERROR);
+    });
+
+    it('return 201{CREATED} , if calls with a valid input', () => {
+      const input: AuthInputDTO = {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+      };
+      return request(app.getHttpServer()).post(AUTH_SIGNUP_PATH).send(input).expect(HttpStatus.CREATED);
     });
   });
 });
